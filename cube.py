db1584ccdd695ec0eb5cec663b1c16fe1f646fc4
Ben Webb <bentenjamin@gmail.com>
2021-02-01 14:21:58 +0200

    u,d moves

--- cube.py
+++ cube.py
@@ -75,6 +75,50 @@ class Cube:
         c[5][8] = c[3][6]
         c[3][6] = t
 
+    def u(self):
+        self.rot_face(4)
+        c = self.cube
+
+        t = c[2][2]
+        c[2][2] = c[3][2]
+        c[3][2] = c[0][2]
+        c[0][2] = c[1][2]
+        c[1][2] = t
+
+        t = c[2][1]
+        c[2][1] = c[3][1]
+        c[3][1] = c[0][1]
+        c[0][1] = c[1][1]
+        c[1][1] = t
+
+        t = c[2][0]
+        c[2][0] = c[3][0]
+        c[3][0] = c[0][0]
+        c[0][0] = c[1][0]
+        c[1][0] = t
+
+    def d(self):
+        self.rot_face(5)
+        c = self.cube
+
+        tempora = c[0][6]
+        c[0][6] = c[3][6]
+        c[3][6] = c[2][6]
+        c[2][6] = c[1][6]
+        c[1][6] = tempora
+
+        tempora = c[0][7]
+        c[0][7] = c[3][7]
+        c[3][7] = c[2][7]
+        c[2][7] = c[1][7]
+        c[1][7] = tempora
+
+        tempora = c[0][8]
+        c[0][8] = c[3][8]
+        c[3][8] = c[2][8]
+        c[2][8] = c[1][8]
+        c[1][8] = tempora
+
     def print_cube(self):
         cube = self.cube
         print(f"           {cube[4][0]} {cube[4][1]} {cube[4][2]}")
@@ -89,5 +133,5 @@ class Cube:
 
 
 cube = Cube()
-cube.b()
+cube.d()
 cube.print_cube()
