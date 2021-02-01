0ce59e385c8c0868210659745bc466d1b702b27e
Ben Webb <bentenjamin@gmail.com>
2021-02-01 13:32:42 +0200

    update on 'f' move function

--- cube.py
+++ cube.py
@@ -1,14 +1,22 @@
 import array
 
+
 class Cube:
     def __init__(self):
-        self.cube = [[11, 12, 13, 14, 15, 16, 17, 18, 19], 
-                    [21, 22, 23, 24, 25, 26, 27, 28, 29], 
-                    [31, 32, 33, 34, 35, 36, 37, 38, 39], 
-                    [41, 42, 43, 44, 45, 46, 47, 48, 49], 
-                    [51, 52, 53, 54, 55, 56, 57, 58, 59], 
-                    [61, 62, 63, 64, 65, 66, 67, 68, 69]]
-        
+        self.cube = [[11, 12, 13, 14, 15, 16, 17, 18, 19],
+                     [21, 22, 23, 24, 25, 26, 27, 28, 29],
+                     [31, 32, 33, 34, 35, 36, 37, 38, 39],
+                     [41, 42, 43, 44, 45, 46, 47, 48, 49],
+                     [51, 52, 53, 54, 55, 56, 57, 58, 59],
+                     [61, 62, 63, 64, 65, 66, 67, 68, 69]]
+
+        self.face_joints = [[5, 2, 6, 4],
+                            [5, 3, 6, 1],
+                            [5, 4, 6, 2],
+                            [5, 1, 6, 3],
+                            [3, 2, 1, 4],
+                            [1, 2, 3, 4]]
+
     def rot_face(self, face_index):
         temp_corner = self.cube[face_index][0]
         self.cube[face_index][0] = self.cube[face_index][6]
@@ -22,6 +30,27 @@ class Cube:
         self.cube[face_index][7] = self.cube[face_index][5]
         self.cube[face_index][5] = temp_edge
 
+    def rep_list(self, slist, dlist):
+        for i in [0, 1, 2]:
+            dlist[i] = slist[i]
+
+    def rot_external(self, cubies):
+        temp_list = cubies[0]
+        self.rep_list(cubies[3], cubies[0])
+        self.rep_list(cubies[2], cubies[3])
+        self.rep_list(cubies[1], cubies[2])
+        self.rep_list(temp_list, cubies[1])
+
+    def f(self):
+        c = self.cube
+        cubies = [[c[4][6], c[4][7], c[4][8]],
+                  [c[1][0], c[1][3], c[1][6]],
+                  [c[5][2], c[5][1], c[5][0]],
+                  [c[3][8], c[3][5], c[3][2]]]
+
+        self.rot_face(0)
+        self.rot_external(cubies)
+
     def print_cube(self):
         cube = self.cube
         print(f"           {cube[4][0]} {cube[4][1]} {cube[4][2]}")
@@ -33,7 +62,8 @@ class Cube:
         print(f"           {cube[5][0]} {cube[5][1]} {cube[5][2]}")
         print(f"           {cube[5][3]} {cube[5][4]} {cube[5][5]}")
         print(f"           {cube[5][6]} {cube[5][7]} {cube[5][8]}")
-    
+
+
 cube = Cube()
-cube.rot_face(0)
+cube.f()
 cube.print_cube()
