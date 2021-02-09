364c446bca56dd4e881d7466f915b60e5aad5576
Ben Webb <bentenjamin@gmail.com>
2021-02-09 16:27:11 +0200

    white corners algo

--- solve.py
+++ solve.py
@@ -1,67 +1,68 @@
 
 
 Right = {
-    "D":"D",
-    "U":"U",
-    "D'":"D'",
-    "U'":"U'",
-    "D2":"D2",
-    "U2":"U2",
-    "R":"B",
-    "L":"F",
-    "F":"R",
-    "B":"L",
-    "R'":"B'",
-    "L'":"F'",
-    "F'":"R'",
-    "B'":"L'",
-    "R2":"B2",
-    "L2":"F2",
-    "F2":"R2",
-    "B2":"L2",
+    "D": "D",
+    "U": "U",
+    "D'": "D'",
+    "U'": "U'",
+    "D2": "D2",
+    "U2": "U2",
+    "R": "B",
+    "L": "F",
+    "F": "R",
+    "B": "L",
+    "R'": "B'",
+    "L'": "F'",
+    "F'": "R'",
+    "B'": "L'",
+    "R2": "B2",
+    "L2": "F2",
+    "F2": "R2",
+    "B2": "L2",
 }
 
 Left = {
-    "D":"D",
-    "U":"U",
-    "D'":"D'",
-    "U'":"U'",
-    "D2":"D2",
-    "U2":"U2",
-    "L":"B",
-    "R":"F",
-    "F":"L",
-    "B":"R",
-    "L'":"B'",
-    "R'":"F'",
-    "F'":"L'",
-    "B'":"R'",
-    "L2":"B2",
-    "R2":"F2",
-    "F2":"L2",
-    "B2":"R2",
+    "D": "D",
+    "U": "U",
+    "D'": "D'",
+    "U'": "U'",
+    "D2": "D2",
+    "U2": "U2",
+    "L": "B",
+    "R": "F",
+    "F": "L",
+    "B": "R",
+    "L'": "B'",
+    "R'": "F'",
+    "F'": "L'",
+    "B'": "R'",
+    "L2": "B2",
+    "R2": "F2",
+    "F2": "L2",
+    "B2": "R2",
 }
 
 Back = {
-    "D":"D",
-    "U":"U",
-    "D'":"D'",
-    "U'":"U'",
-    "D2":"D2",
-    "U2":"U2",
-    "R":"L",
-    "L":"R",
-    "F":"B",
-    "B":"F",
-    "R'":"L'",
-    "L'":"R'",
-    "F'":"B'",
-    "B'":"F'",
-    "R2":"L2",
-    "L2":"R2",
-    "F2":"B2",
-    "B2":"F2",
+    "D": "D",
+    "U": "U",
+    "D'": "D'",
+    "U'": "U'",
+    "D2": "D2",
+    "U2": "U2",
+    "R": "L",
+    "L": "R",
+    "F": "B",
+    "B": "F",
+    "R'": "L'",
+    "L'": "R'",
+    "F'": "B'",
+    "B'": "F'",
+    "R2": "L2",
+    "L2": "R2",
+    "F2": "B2",
+    "B2": "F2",
 }
+
 
 class Algos:
     # get the side of the correlated coords [x, z]
@@ -105,7 +106,14 @@ class Algos:
         "W": "U",
         "Y": "D"
     }
-    
+
+    right_col_of_face = {
+        (0, 0): "O",
+        (0, 2): "G",
+        (2, 0): "B",
+        (2, 2): "R"
+    }
+
     def __init__(self, cube):
         # self.cube = c
         self.cube = cube
@@ -115,37 +123,65 @@ class Algos:
     def write_exe_moves(self, moves):
         self.moves.extend(moves)
         self.cube.do_moves(moves)
-    
+        print(moves)
+        print(self.cube)
+
     def cross(self):
         c = self.c
-        mid_layer_white_edge = {
-            (0, 0): "O",
-            (0, 2): "G",
-            (2, 0): "B",
-            (2, 2): "R"
-        }
 
         for colour in ["R", "B", "O", "G"]:
             cubie = self.cube.find_cubie([colour, "W", "N"])
 
             # if cubie is on the white face
             if (cubie.point[1] == 2):
-                self.write_exe_moves([self.coord_to_side[(cubie.point[0], cubie.point[2])] + "2"])
+                # if cubie.colours[1] == "W" and self.cube.pieces[cubie.point[0]][2][cubie.point[2]] == cubie:
+                #         continue
+                self.write_exe_moves(
+                    [self.coord_to_side[(cubie.point[0], cubie.point[2])] + "2"])
             # if cubie is in the middle
             if (cubie.point[1] == 1):
                 # this is borked need to find which face it is on
-                self.write_exe_moves(move_translator(mid_layer_white_edge[(cubie.point[0], cubie.point[2])], ["F", "D", "F'"]))
+                self.write_exe_moves(move_translator(
+                    self.right_col_of_face[(cubie.point[0], cubie.point[2])], ["F", "D", "F'"]))
             # change to use point
-            center = self.c[(self.c_t_c[colour])[0]][1][(self.c_t_c[colour])[1]]
+            center = self.c[(self.c_t_c[colour])[0]
+                            ][1][(self.c_t_c[colour])[1]]
             # change to use point
             while not self.c[self.c_t_c[colour][0]][0][self.c_t_c[colour][1]] == cubie:
                 self.write_exe_moves(["D"])
             if cubie.colours[1] == "W":
                 self.write_exe_moves([move_translator(colour, "F")[0] + "2"])
             else:
-                self.write_exe_moves(move_translator(colour, "F' U' R U".split()))
+                self.write_exe_moves(move_translator(
+                    colour, "F' U' R U".split()))
 
-#up and down not included here
+    def white_corner_helper(self, xy, moves):
+        self.write_exe_moves(move_translator(
+            self.right_col_of_face[xy], moves))
+
+    def white_corners(self):
+        for x in [0, 2]:
+            for z in [0, 2]:
+                cubie = self.cube.pieces[x][2][z]
+
+                if cubie.point[1] == 2:
+                    if (cubie.colours[1] == "W") and (self.cube.cube[x][2][z] is cubie):
+                        continue
+                    self.white_corner_helper((cubie.point[0], cubie.point[2]), ["R'", "D'", "R"])
+
+                while not ((cubie.point[0] == x) and (cubie.point[2] == z)):
+                    self.write_exe_moves(['D'])
+
+                if cubie.colours[1] == "W":
+                    self.white_corner_helper((x, z), ["R'", "D", "R", "D2"])
+
+                if (cubie.colours[2] == "W" and x == z) or (cubie.colours[0] == "W" and x != z):
+                    self.white_corner_helper((x, z), ["F", "D", "F'"])
+                else:
+                    self.white_corner_helper((x, z), ["R'", "D'", "R"])
+
+
+# up and down not included here
 def move_translator(face, moves):
     new_moves = []
     for move in moves:
@@ -166,10 +202,12 @@ def move_translator(face, moves):
 
 # print(move_translator("L", "R'"))
 
-def solve(cube):
-        algos = Algos(cube)
-        
-        algos.cross()
 
-        print(cube)
-        print(algos.moves)
+def solve(cube):
+    algos = Algos(cube)
+
+    algos.cross()
+    algos.white_corners()
+
+    print(cube)
+    print(algos.moves)
